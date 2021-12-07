420c26fbb617dd9579d18541c44d690db129aaf1
Leref <12578483+Leref@users.noreply.github.com>
2021-12-06 23:34:16 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const plugins = require("./src/Utility/import.js")
 
 module.exports = {
-    $leref: plugins.leref,
-    $dbdts: plugins.dbdts
+    leref: plugins.leref,
+    dbdts: plugins.dbdts
 }
